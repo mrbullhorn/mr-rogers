1a1fda197c53a69464c98357687db75bd5ea3fb9
Hans Ellis <bullhorn@gmail.com>
2022-09-08 15:03:58 -0700

    logic passes test 5

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,38 @@ function countUp(userInput) {
   const ourArray = []
   for (index = 0; index <= ourNum; index += 1) {
     ourArray.push(0 + index);
-    }
+    
   }
   ourArray.forEach(function(number) {
-      ourArray.splice(number -1,  1, "Beep!")
-  });
+      stringNum = number.toString()
+      if (stringNum.includes(1)) {
+      ourArray.splice(stringNum,  1, "Beep!")
+      }
+    });
+  return ourArray;
+  }
 }
 
+//experimental
 
-
-
+function countUp(userInput) {
+  let ourNum = parseInt(userInput)
+  if (isNaN(ourNum)) {
+    return "Please Enter a Number.";
+  } else {
+  const ourArray = []
+  for (index = 0; index <= ourNum; index += 1) {
+    ourArray.push(0 + index);
+    
+  }
+  ourArray.forEach(function(number) {
+      stringNum = number.toString()
+      if (stringNum.includes("2")) {
+        ourArray.splice(stringNum, 1, "Boop!")
+      } else if (stringNum.includes("1")) {
+      ourArray.splice(stringNum,  1, "Beep!")
+      }
+    });
+  return ourArray;
+  }
+}
