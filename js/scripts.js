c89287674272fe1263f20faeb8c938f891233f75
Hans Ellis <bullhorn@gmail.com>
2022-09-08 15:35:07 -0700

    complete mvp of business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,26 @@
+//business logic
 function countUp(userInput) {
   let ourNum = parseInt(userInput)
   if (isNaN(ourNum)) {
     return "Please Enter a Number.";
-  } else {
+  }
   const ourArray = []
   for (index = 0; index <= ourNum; index += 1) {
     ourArray.push(0 + index);
-    
-  }
-  ourArray.forEach(function(number) {
-      stringNum = number.toString()
-      if (stringNum.includes(1)) {
-      ourArray.splice(stringNum,  1, "Beep!")
-      }
-    });
-  return ourArray;
-  }
-}
-
-//experimental
-
-function countUp(userInput) {
-  let ourNum = parseInt(userInput)
-  if (isNaN(ourNum)) {
-    return "Please Enter a Number.";
-  } else {
-  const ourArray = []
-  for (index = 0; index <= ourNum; index += 1) {
-    ourArray.push(0 + index);
-    
   }
   ourArray.forEach(function(number) {
       stringNum = number.toString();
-      
       if (stringNum.includes(3)) {
         ourArray.splice(stringNum, 1, "Won't you be my neighbor?");
       } else if (stringNum.includes("2")) {
         ourArray.splice(stringNum, 1, "Boop!");
-      } else (stringNum.includes("1")) {
+      } else if (stringNum.includes("1")) {
       ourArray.splice(stringNum,  1, "Beep!");
       }
     });
   return ourArray;
-  }
 }
+
+
+//////
+
