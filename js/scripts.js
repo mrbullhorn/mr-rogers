83de55bce530f702fb414e5bc23e36de52ce1d5a
Hans Ellis <bullhorn@gmail.com>
2022-09-08 12:00:02 -0700

    create countUp logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
 function countUp(userInput) {
   let ourNum = parseInt(userInput)
+  if (isNaN(ourNum)) {
+    return "Please Enter a Number.";
+  } else {
   const ourArray = []
   for (index = 0; index <= ourNum; index += 1) {
     ourArray.push(0 + index);
   } 
 return ourArray;
+  }
 }
+
